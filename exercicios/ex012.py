99a25b3fa0e9f5818405e7fe35f27f7fa0c8c5f0
fabricio-esper <fabricio_esper@outlook.com>
2021-01-25 21:54:58 -0300

    Adicionado ex012.py e ex013.py

---
+++ exercicios/ex012.py
@@ -0,0 +1,3 @@
+p = float(input("Insira o preço do produto R$"))
+d = (p*0.05)
+print(f"O produto que custava R${p:.2f} com um desconto de 5% passou a custar {p-d:.2f}")
---
+++ exercicios/ex013.py
@@ -0,0 +1,2 @@
+s = float(input("Qual é o salário? R$"))
+print(f"Seu novo salário com 15% de aumento é de R${s+(s*0.15):.2f}")
