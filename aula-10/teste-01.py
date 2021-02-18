c91ac86c99d5649e4d85463ba3914f53ee5fa748
fabricio-esper <fabricio_esper@outlook.com>
2021-02-18 16:25:38 -0300

    Adicionado aula-10/

---
+++ aula-10/teste-01.py
@@ -0,0 +1,8 @@
+tempo = int(input('Quantos anos tem seu carro? '))
+print('carro novo' if tempo <=3 else 'carro velho')
+print('--FIM--')
+
+#if <=3:
+#   print('carro novo')
+#else:
+#   print('carro velho')
